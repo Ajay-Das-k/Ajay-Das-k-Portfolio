674572351c841a9a38d30be257997794344a4810
Ajay-Das-k <ajtechhub01@gmail.com>
2024-04-03 11:05:56 +0530

    </ToggleButton>

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -12,47 +12,90 @@ const Projects = ({openModal,setOpenModal}) => {
       <Wrapper>
         <Title>Projects</Title>
         <Desc>
-          I have worked on a wide range of projects. From web apps to android apps. Here are some of my projects.
+          I have worked on a wide range of projects. From web apps to IOT. Here
+          are some of my projects.
         </Desc>
-        <ToggleButtonGroup >
-          {toggle === 'all' ?
-            <ToggleButton active value="all" onClick={() => setToggle('all')}>All</ToggleButton>
-            :
-            <ToggleButton value="all" onClick={() => setToggle('all')}>All</ToggleButton>
-          }
+        <ToggleButtonGroup>
+          {toggle === "all" ? (
+            <ToggleButton active value="all" onClick={() => setToggle("all")}>
+              All
+            </ToggleButton>
+          ) : (
+            <ToggleButton value="all" onClick={() => setToggle("all")}>
+              All
+            </ToggleButton>
+          )}
           <Divider />
-          {toggle === 'web app' ?
-            <ToggleButton active value="web app" onClick={() => setToggle('web app')}>WEB APP'S</ToggleButton>
-            :
-            <ToggleButton value="web app" onClick={() => setToggle('web app')}>WEB APP'S</ToggleButton>
-          }
+          {toggle === "web app" ? (
+            <ToggleButton
+              active
+              value="web app"
+              onClick={() => setToggle("web app")}
+            >
+              FRONT END
+            </ToggleButton>
+          ) : (
+            <ToggleButton value="web app" onClick={() => setToggle("web app")}>
+              FRONT END
+            </ToggleButton>
+          )}
           <Divider />
-          {toggle === 'android app' ?
-            <ToggleButton active value="android app" onClick={() => setToggle('android app')}>ANDROID APP'S</ToggleButton>
-            :
-            <ToggleButton value="android app" onClick={() => setToggle('android app')}>ANDROID APP'S</ToggleButton>
-          }
+          {toggle === "android app" ? (
+            <ToggleButton
+              active
+              value="android app"
+              onClick={() => setToggle("android app")}
+            >
+              BACKEND
+            </ToggleButton>
+          ) : (
+            <ToggleButton
+              value="android app"
+              onClick={() => setToggle("android app")}
+            >
+              BACKEND
+            </ToggleButton>
+          )}
           <Divider />
-          {toggle === 'machine learning' ?
-            <ToggleButton active value="machine learning" onClick={() => setToggle('machine learning')}>MACHINE LEARNING</ToggleButton>
-            :
-            <ToggleButton value="machine learning" onClick={() => setToggle('machine learning')}>MACHINE LEARNING</ToggleButton>
-          }
+          {toggle === "machine learning" ? (
+            <ToggleButton
+              active
+              value="machine learning"
+              onClick={() => setToggle("machine learning")}
+            >
+              MINI PROJECT
+            </ToggleButton>
+          ) : (
+            <ToggleButton
+              value="machine learning"
+              onClick={() => setToggle("machine learning")}
+            >
+              MINI PROJECT
+            </ToggleButton>
+          )}
         </ToggleButtonGroup>
         <CardContainer>
-          {toggle === 'all' && projects
-            .map((project) => (
-              <ProjectCard project={project} openModal={openModal} setOpenModal={setOpenModal}/>
+          {toggle === "all" &&
+            projects.map((project) => (
+              <ProjectCard
+                project={project}
+                openModal={openModal}
+                setOpenModal={setOpenModal}
+              />
             ))}
           {projects
             .filter((item) => item.category == toggle)
             .map((project) => (
-              <ProjectCard project={project} openModal={openModal} setOpenModal={setOpenModal}/>
+              <ProjectCard
+                project={project}
+                openModal={openModal}
+                setOpenModal={setOpenModal}
+              />
             ))}
         </CardContainer>
       </Wrapper>
     </Container>
-  )
+  );
 }
 
 export default Projects
