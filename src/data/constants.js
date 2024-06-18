5824c3463654e39bdd5864aca5ab6aeb8c0e19e8
Ajay-Das-k <ajtechhub01@gmail.com>
2024-06-18 15:46:54 +0530

    second

--- src/data/constants.js
+++ src/data/constants.js
@@ -26,6 +26,15 @@ export const skills = [
     skills: [
       {
         name: "React Js",
+        image: "https://www.awesomelwc.com/resources/lwc.png",
+      },
+      {
+        name: "AURA",
+        image:
+          "https://res.cloudinary.com/hy4kyit2a/f_auto,fl_lossy,q_70/learn/projects/quick-start-lightning-web-components/97fa54b90c579eaa8b4e63b80588679e_badge.png",
+      },
+      {
+        name: "LWC",
         image:
           "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
       },
